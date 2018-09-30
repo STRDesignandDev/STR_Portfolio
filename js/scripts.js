409493e6cd2873f71a4dfe1c2ce0f278e0108514
Ryan Strawn <str.designanddev@gmail.com>
2018-09-29 19:02:31 -0600

    initial push

---
+++ js/scripts.js
@@ -0,0 +1,200 @@
+// NAVIGATION
+
+var page_isolated = false;
+
+
+
+$( "#projectslink" ).on('click', function() {
+    $('#projectssection').delay(800).fadeIn();
+    $('#aboutsection').fadeOut();
+    $('#contactsection').fadeOut();
+    $('.workcategories').hide(500);
+    $('.introcontent').hide(500);
+    $('.introcontent2').hide(500);
+    $('#heading_sticker1').delay(800).fadeIn();
+    $('#heading_sticker2').fadeOut();
+    $('#heading_sticker3').fadeOut();
+    page_isolated = true;
+    $('.section_wrapper').css('padding-top', '0px');
+});
+
+$( "#aboutlink" ).on('click', function() {
+    $('#projectssection').fadeOut();
+    $('#contactsection').fadeOut();
+    $('#aboutsection').delay(800).fadeIn();
+    $('.workcategories').hide(500);
+    $('.introcontent').hide(500);
+    $('.introcontent2').hide(500);
+    $('#heading_sticker1').fadeOut();
+    $('#heading_sticker2').delay(800).fadeIn();
+    $('#heading_sticker3').fadeOut();
+    page_isolated = true;
+    $('.section_wrapper').css('padding-top', '0px');
+});
+
+$( "#contactlink" ).on('click', function() {
+    $('#contactsection').delay(800).fadeIn();
+    $('#projectssection').fadeOut();
+    $('#aboutsection').fadeOut();
+    $('.workcategories').hide(500);
+    $('.introcontent').hide(500);
+    $('.introcontent2').hide(500);
+    $('#heading_sticker1').fadeOut();
+    $('#heading_sticker2').fadeOut();
+    $('#heading_sticker3').delay(800).fadeIn();
+    page_isolated = true;
+    $('.section_wrapper').css('padding-top', '0px');
+});
+
+$( "#homelink" ).on('click', function() {
+    $('#projectssection').delay(400).show(500);
+    $('#aboutsection').delay(400).show(500);;
+    $('#contactsection').delay(400).show(500);
+    $('.workcategories').show(500);
+    $('.introcontent').hide();
+    $('.introcontent2').show(500);
+    $('#heading_sticker1').show();
+    $('#heading_sticker2').show();
+    $('#heading_sticker3').show();
+    page_isolated = false;
+    $('.section_wrapper').css('padding-top', '100px');
+});
+
+// Set up listeners for screen size
+
+var x = window.matchMedia("(max-width: 599px)")
+screenSm(x) // Call listener function at run time
+x.addListener(screenSm) // Attach listener function on state changes
+
+var y = window.matchMedia("(min-width: 600px)")
+screenLg(y) // Call listener function at run time
+y.addListener(screenLg) // Attach listener function on state changes
+
+// Nav changes for large screens
+
+function screenLg(y) {
+    if (y.matches) {
+      $('.rightmenu').css("right", '0').show();
+      $('.hamburger').hide();
+    }
+}
+
+// Nav changes for small screens
+
+function screenSm(x) {
+    if (x.matches) {
+      $('.hamburger').show();
+      $('.rightmenu').css("right", '-30%');  // resets the position of the menu on state change of screen width
+  }
+}
+
+// Animate menu in from right side when on small screens
+
+$(document).on('click', '.menurightitem', function() {
+      $('.rightmenu').show();
+      if (x.matches){
+        $('.rightmenu').animate({"right": '-=30%'});
+        return false;
+      }
+});
+
+// animate menu out of view screen for small screens
+
+$(document).on( "click touchend", ".hamburger", function() {
+  $('.rightwrapper').animate({"left": '+=10%'}, "med" );
+  $('.rightmenu').delay(400).animate({"right": '+=30%'}, "fast");
+});
+
+
+// Scroll magic stuff
+
+// Intro work category icons fade on scroll down
+
+var controller = new ScrollMagic.Controller();
+
+// build scene
+
+var scene = new ScrollMagic.Scene({ triggerElement: "#trigger1", offset: 40})
+        // trigger a velocity opacity animation
+        .setVelocity("#animate1", {opacity: 0}, {duration: 200})
+        // .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger2", offset: 90})
+        // trigger a velocity opaticy animation
+        .setVelocity("#animate2", {opacity: 0}, {duration: 200})
+        // .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger3", offset: 140})
+        // trigger a velocity opaticy animation
+        .setVelocity("#animate3", {opacity: 0}, {duration: 200})
+        // .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+var scene = new ScrollMagic.Scene({triggerElement: "#trigger4", offset: 190})
+        // trigger a velocity opacity animation
+        .setVelocity("#animate4", {opacity: 0}, {duration: 200})
+        // .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+
+// Sticky headers
+
+
+
+window.addEventListener('scroll', function() {
+
+    if (page_isolated == false){
+
+     var header1 = document.getElementById('heading_sticker1');
+     var distanceToTopProject = header1.getBoundingClientRect().top;
+
+     var header2 = document.getElementById('heading_sticker2');
+     var distanceToTopAbout = header2.getBoundingClientRect().top;
+
+
+     var header3 = document.getElementById('heading_sticker3');
+     var distanceToTopContact = header3.getBoundingClientRect().top;
+
+       if (distanceToTopProject < 50){
+         $('#project_stick').addClass('sticky');
+         $('#projectssection').addClass('padme');
+       }
+       else {
+         $('#project_stick').removeClass('sticky');
+         $('#projectssection').removeClass('padme');
+       }
+       if (distanceToTopAbout < 50){
+         $('#about_stick').addClass('sticky');
+         $('#aboutsection').addClass('padme');
+       }
+       else {
+         $('#about_stick').removeClass('sticky');
+         $('#aboutsection').removeClass('padme');
+       }
+       if (distanceToTopContact < 50){
+         $('#contact_stick').addClass('sticky');
+         $('#contactsection').addClass('padme');
+       }
+       else {
+         $('#contact_stick').removeClass('sticky');
+         $('#contactsection').removeClass('padme');
+       }
+     }
+  });
+
+
+
+// Skills table animate1
+window.addEventListener('scroll', function() {
+
+  var skills_table = document.getElementById('skills_table');
+  var distanceToTopSkillsTable = skills_table.getBoundingClientRect().top;
+
+  if (distanceToTopSkillsTable < 300){
+      $("#html5").animate({width: '90%'});
+      $("#css").delay(100).animate({width: '80%'});
+      $("#ruby").delay(200).animate({width: '50%'});
+      $("#jQuery").delay(300).animate({width: '70%'});
+      $("#javascript").delay(400).animate({width: '40%'});
+      $("#adobe").delay(500).animate({width: '80%'});
+    }
+});
